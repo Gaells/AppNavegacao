e7799e18352d88c603195b1d9c84c482d96d0c1a
Gabriel Chioquetta <gabriel.obladen@gmail.com>
2024-06-08 19:12:19 -0300

    add img´s

--- src/consts/categories.tsx
+++ src/consts/categories.tsx
@@ -7,7 +7,7 @@ interface Category {
 const categories: Category[] = [
   { id: '1', name: 'Combos', image: require('../assets/categories/combo.png') },
   { id: '2', name: 'Hamburger', image: require('../assets/categories/burger.png') },
-  { id: '3', name: 'Bebidas', image: require('../assets/categories/drink.png') },
+  { id: '3', name: 'Bebidas', image: require('../assets/categories/drink.jpg') },
   { id: '4', name: 'Sobremesas', image: require('../assets/categories/sobremesa.png') },
 ];
 
